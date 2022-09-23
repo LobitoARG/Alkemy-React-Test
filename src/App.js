78827ee97c540ba318ddc522b2ff6f7318ebed98
Alejandro Jabo <ajabo@multitech.com.ar>
2022-09-23 17:52:26 -0300

    Unidad 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Login from "./components/Login";
 import Listado from "./components/Listado";
 import { Routes, Route } from 'react-router-dom'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+//CSS
+import './css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <Header/>
       <Routes>
         <Route exact path="/" element={<Login/>}/>
         <Route path="/listado" element={<Listado/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+function Footer(){
+    return(
+        <>
+        <footer>
+            <p>Copyright TM Lobito</p>
+            <nav>
+                <ul>
+                    <li>
+                    <a href="https://www.instagram.com" rel="noopener noreferrer">Instagram</a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
